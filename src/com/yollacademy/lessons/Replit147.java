dd7e6601fb992a466bc869ea2a0a5b067e078182
cryptusdev <cryptusdev@gmail.com>
2020-05-22 02:42:20 -0500

    Update replit 147

--- src/com/yollacademy/lessons/Replit147.java
+++ src/com/yollacademy/lessons/Replit147.java
@@ -7,17 +7,18 @@ public class Replit147 {
         Scanner inp = new Scanner(System.in);
         int size = inp.nextInt();
         int[] nums = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = inp.nextInt();
+        }
         isPos(nums);
 
     }
 
     public static void isPos(int[] arr) {
         for (int i = 0; i < arr.length; i++){
-            if (arr[i] <= 0) {
+            if (arr[i] < 0) {
                 System.out.println("not positive");
-            }else{
-                System.out.println("positive");
-            }
+            }else System.out.println("positive");
         }
 
     }
