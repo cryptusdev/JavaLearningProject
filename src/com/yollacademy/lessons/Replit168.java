35444f098cd36f96d43b10cf7724a74cbe805d14
h-hhasan <h.humbatov@gmail.com>
2020-06-08 22:22:31 -0500

    June 8 replit updates

---
+++ src/com/yollacademy/lessons/Replit168.java
@@ -0,0 +1,32 @@
+package com.yollacademy.lessons;
+
+import java.util.Scanner;
+
+public class Replit168 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter main:");
+//        String word = input.next();
+//        System.out.println("Enter coverMe:");
+//        String part = input.next();
+//
+//        System.out.println(coverString(word, part));
+//
+//    }
+//    public static String coverString(String word, String part){
+//        String result = "";
+//
+//        if (word.indexOf(part) == -1) {
+//            result += "[" + word + "]";
+//        }else{
+//            result +=
+//        }
+//
+//        while (word.contains(part)) {
+//            result += "[" + word.substring(0)
+//        }
+//
+//
+//        return result;
+//    }
+}
--- src/com/yollacademy/lessons/Test.java
+++ src/com/yollacademy/lessons/Test.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int[] x = new int [] {2, 9, 4, 6, 8, 10, 3, 5, 7, 9};
-        boolean isUnique=true;
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                if (i != j) {
-                    if (x[i] == x[j]) {
-                        isUnique=false;
-                    }
-                }
-            }
-        }
-        System.out.println(isUnique);
+//        String word = "mejavamethod";
+//        String part = "me";
+//        String result;
+//        System.out.println("[" + word.substring(word.indexOf(part), part.length()) + "]" + word.substring());
+//        if (word.indexOf(part) == -1) {
+//            System.out.println("[" + word + "]");
+//        }
+//        else if (word.indexOf(part) == 0) {
+//            System.out.println("[" + word.substring(word.indexOf(part), word.length()) + "]");
+//        }
+
 
     }
 }
