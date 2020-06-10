d20f708edd34efe1d0b6a03bb5c8c5d71901c0c3
h-hhasan <h.humbatov@gmail.com>
2020-06-09 21:56:57 -0500

    June 8 replit updates

---
+++ src/com/yollacademy/lessons/Db.java
@@ -0,0 +1,17 @@
+package com.yollacademy.lessons;
+
+public class Db {
+    private static String data;
+    private static int yint;
+
+    public static void insertData(String eData, int eInt) {
+        data = eData;
+        yint = eInt;
+    }
+
+    public String getData() { return data; }
+    public void setData(String data) { this.data = data; }
+
+    public int getYint() { return yint; }
+    public void setYint(int yint) { this.yint = yint; }
+}
